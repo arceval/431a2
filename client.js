43c624cf56bf4cb110a95b120e2723ffe4677f76
Rohm Laxton <rohmkl@gmail.com>
2019-02-08 19:37:25 +0000

    Adding client

---
+++ client.js
@@ -0,0 +1,29 @@
+const http = require('http');
+var options = {
+  host: '10.138.0.2',
+  port: 3000,
+  path: '/time',
+  method: 'GET'
+}
+
+var beforeTimestamp = new Date();
+var returnedTime;
+http.request(options, function(res) {
+  // Server object has been received, timestamp end
+  var afterTimestamp = new Date();
+  res.setEncoding('utf8');
+  res.on('data', function (chunk){
+    var jsonObject = JSON.parse(chunk);
+    returnedTime = jsonObject.time;
+    var serverTime = new Date(returnedTime + ((afterTimestamp - beforeTimestamp) / 2));
+    console.log("Server time: " + serverTime);
+    // Validation uncomment 
+    /*
+    console.log("Server time: " + serverTime.getTime());
+    console.log("Before: " + beforeTimestamp.getTime());
+    console.log("After: " + afterTimestamp.getTime());
+    console.log("Diff: " + (afterTimestamp.getTime() - beforeTimestamp.getTime()));
+    console.log("Server val without rtt: " + returnedTime );
+    */
+  })
+}).end();
